b93c0437bb946d5aebd2aa3daeea86e2ac78c7fd
Hunter Beckmann <i54965@xactware.com>
2016-06-02 13:39:37 -0600

    date sorting and avatar formating

--- app.js
+++ app.js
@@ -2,17 +2,32 @@ angular.module('SocialUI', [])
   .controller('SocialController', ['$http', function($http) {
 
     var self = this;
-    self.test = "Hello World";
     self.activities;
+
     self.getActivities = function() {
       $http.get('https://nuvi-challenge.herokuapp.com/activities')
       .then(function(res) {
-        console.dir(res.data);
         self.activities = res.data;
+        self.sortDate();
+        console.dir(self.activities);
       },
       function(err){
         console.log("An error has occured when retriveing activity: " + err)
       })
+    };
+
+    self.sortDate = function() {
+
+      self.activities.map(function(obj) {
+        var rawDate = obj.activity_date.split("-").join("");
+        obj.activity_date_raw = rawDate;
+        return obj;
+      });
+
+      self.activities.sort(function(a, b) {
+        return a.activity_date_raw - b.activity_date_raw;
+      });
+
     };
 
     self.getActivities();
